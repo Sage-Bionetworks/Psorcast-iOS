b8647f99ddd473dbb4cbb1950219e6414116b215
Khai Do <zaro0508@gmail.com>
2019-07-21 22:49:54 -0700

    setup for travis ci
    * Setup travis configuration
    * Setup fastlane for command line builds
    * Add script to manage developer workflow (build on PR and Merges)
    * Change project to manually manage profiles (a fastlane required config)

---
+++ travis/build.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+set -ex
+if [[ "$TRAVIS_PULL_REQUEST" != "false" ]]; then     # on pull requests
+    echo "Build on PR"
+    FASTLANE_EXPLICIT_OPEN_SIMULATOR=2 bundle exec fastlane test scheme:"Psorcast"
+elif [[ -z "$TRAVIS_TAG" && "$TRAVIS_BRANCH" == "master" ]]; then  # non-tag commits to master branch
+    echo "Build on merge to master"
+    FASTLANE_EXPLICIT_OPEN_SIMULATOR=2 bundle exec fastlane test scheme:"Psorcast"
+    bundle exec fastlane keychains
+    bundle exec fastlane certificates
+    # bundle exec fastlane archive scheme:"Psorcast" export_method:"ad-hoc"
+    bundle exec fastlane archive scheme:"Psorcast" export_method:"ad-hoc" project:"Psorcast/Psorcast.xcodeproj"
+elif [[ -z "$TRAVIS_TAG" && "$TRAVIS_BRANCH" =~ ^stable-.* ]]; then # non-tag commits to stable branches
+    echo "Build on stable branch"
+    FASTLANE_EXPLICIT_OPEN_SIMULATOR=2 bundle exec fastlane test scheme:"Psorcast"
+    bundle exec fastlane bump_all
+    bundle exec fastlane keychains
+    bundle exec fastlane certificates
+    # bundle exec fastlane archive scheme:"Psorcast" export_method:"ad-hoc"
+    bundle exec fastlane archive scheme:"Psorcast" export_method:"ad-hoc" project:"Psorcast/Psorcast.xcodeproj"
+fi
+exit $?
