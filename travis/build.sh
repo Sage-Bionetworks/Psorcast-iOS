ca5f342ba784fda78e19a93e40f143a96c2c83fb
Khai Do <zaro0508@gmail.com>
2019-07-23 09:58:16 -0700

    Add psorcastValidation0 to build
    * Add org.sageBionetworks.psorcastValidation0 certificates to ios-certifcates repo[1]
    * Setup fastlane to install org.sageBionetworks.psorcastValidation0 profiles
    * Add checkout of iOSPrivateProjectInfo repo[2] for tests
    * Change psorcastValidation to manually manage profiles (a fastlane required config)
    
    [1] https://github.com/Sage-Bionetworks/ios-certificates/tree/KA9Z8R6M6K/profiles/adhoc
    [2] https://github.com/Sage-Bionetworks/iOSPrivateProjectInfo

--- travis/build.sh
+++ travis/build.sh
@@ -2,23 +2,29 @@
 set -ex
 if [[ "$TRAVIS_PULL_REQUEST" != "false" ]]; then     # on pull requests
     echo "Build on PR"
+    git clone https://github.com/Sage-Bionetworks/iOSPrivateProjectInfo.git ../iOSPrivateProjectInfo
     FASTLANE_EXPLICIT_OPEN_SIMULATOR=2 bundle exec fastlane test scheme:"Psorcast"
+    FASTLANE_EXPLICIT_OPEN_SIMULATOR=2 bundle exec fastlane test scheme:"PsorcastValidation"
 elif [[ -z "$TRAVIS_TAG" && "$TRAVIS_BRANCH" == "master" ]]; then  # non-tag commits to master branch
     echo "Build on merge to master"
-    FASTLANE_EXPLICIT_OPEN_SIMULATOR=2 bundle exec fastlane test scheme:"Psorcast"
+    git clone https://github.com/Sage-Bionetworks/iOSPrivateProjectInfo.git ../iOSPrivateProjectInfo
+    # FASTLANE_EXPLICIT_OPEN_SIMULATOR=2 bundle exec fastlane test scheme:"Psorcast"
+    FASTLANE_EXPLICIT_OPEN_SIMULATOR=2 bundle exec fastlane test scheme:"PsorcastValidation"
     bundle exec fastlane keychains
     bundle exec fastlane certificates
     bundle exec fastlane noprompt
-    # bundle exec fastlane archive scheme:"Psorcast" export_method:"ad-hoc"
-    bundle exec fastlane ci_archive scheme:"Psorcast" export_method:"ad-hoc" project:"Psorcast/Psorcast.xcodeproj"
+    bundle exec fastlane archive scheme:"Psorcast" export_method:"ad-hoc"
+    bundle exec fastlane ci_archive scheme:"PsorcastValidation" export_method:"ad-hoc" project:"Psorcast/Psorcast.xcodeproj"
 elif [[ -z "$TRAVIS_TAG" && "$TRAVIS_BRANCH" =~ ^stable-.* ]]; then # non-tag commits to stable branches
     echo "Build on stable branch"
+    git clone https://github.com/Sage-Bionetworks/iOSPrivateProjectInfo.git ../iOSPrivateProjectInfo
     FASTLANE_EXPLICIT_OPEN_SIMULATOR=2 bundle exec fastlane test scheme:"Psorcast"
+    FASTLANE_EXPLICIT_OPEN_SIMULATOR=2 bundle exec fastlane test scheme:"PsorcastValidation"
     bundle exec fastlane bump_all
     bundle exec fastlane keychains
     bundle exec fastlane certificates
     bundle exec fastlane noprompt
-    # bundle exec fastlane archive scheme:"Psorcast" export_method:"ad-hoc"
-    bundle exec fastlane ci_archive scheme:"Psorcast" export_method:"ad-hoc" project:"Psorcast/Psorcast.xcodeproj"
+    bundle exec fastlane archive scheme:"Psorcast" export_method:"ad-hoc"
+    bundle exec fastlane ci_archive scheme:"PsorcastValidation" export_method:"ad-hoc" project:"Psorcast/Psorcast.xcodeproj"
 fi
 exit $?
