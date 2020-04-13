6e2dcf312cb4c3f3188cbf99164abf7924acc6f8
Eric Sieg <eric@sdpdigital.com>
2020-04-13 01:41:10 -0700

    center buttons, connect to IBOutlet

--- Psorcast/Psorcast/ShowInsightStepViewController.swift
+++ Psorcast/Psorcast/ShowInsightStepViewController.swift
@@ -38,6 +38,28 @@ import BridgeApp
 import BridgeAppUI
 import GPUImage
 
+class HorizontallyCenteredButton: UIButton {
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        self.contentHorizontalAlignment = .left
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        self.centerButtonImageAndTitle()
+    }
+
+    func centerButtonImageAndTitle() {
+        let size = self.bounds.size
+        let titleSize = self.titleLabel!.frame.size
+        let imageSize = self.imageView!.frame.size
+
+        self.imageEdgeInsets = UIEdgeInsets(top: self.imageEdgeInsets.top, left: size.width/2 - imageSize.width/2, bottom: self.imageEdgeInsets.bottom, right: 0)
+        self.titleEdgeInsets = UIEdgeInsets(top: self.titleEdgeInsets.top, left: -imageSize.width + size.width/2 - titleSize.width/2, bottom: self.titleEdgeInsets.bottom, right: 0)
+    }
+}
+
+
 open class ShowInsightStepObject: RSDUIStepObject, RSDStepViewControllerVendor {
     
     private enum CodingKeys: String, CodingKey, CaseIterable {
@@ -80,6 +102,8 @@ open class ShowInsightStepObject: RSDUIStepObject, RSDStepViewControllerVendor {
 
 public class ShowInsightStepViewController: RSDStepViewController {
 
+    @IBOutlet weak var noButton: HorizontallyCenteredButton!
+    @IBOutlet weak var yesButton: HorizontallyCenteredButton!
     
 }
 
