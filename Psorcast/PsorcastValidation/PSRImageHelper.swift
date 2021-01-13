b1e30762882e1f19fde8072c93532ba42dd4e11a
MDP <mike@sdpdigital.com>
2021-01-12 18:22:28 -0800

    Increased efficiency of percent coverage calculation

--- Psorcast/PsorcastValidation/PSRImageHelper.swift
+++ Psorcast/PsorcastValidation/PSRImageHelper.swift
@@ -228,6 +228,22 @@ extension UIImage {
         return newImage!
     }
     
+    func resizeImageAspectFit(toTargetWidthInPixels: CGFloat) -> UIImage {
+        guard let cgImage = self.cgImage else {
+            return self
+        }
+        
+        // Target width is in pixels, not points, so calculate density
+        let density = CGFloat(self.size.width) / CGFloat(cgImage.width)
+        // And apply that density to the target pixels to make all images the same true width
+        let targetWidthInPts = toTargetWidthInPixels * density
+        // Compute the aspect ratio of the image
+        let aspectRatio = CGFloat(cgImage.height) / CGFloat(cgImage.width)
+        let targetSizeInPts = CGSize(width: targetWidthInPts, height: aspectRatio * targetWidthInPts)
+                
+        return self.resizeImage(targetSize: targetSizeInPts)
+    }
+    
     func cropImage(rect: CGRect) -> UIImage {
         var rect = rect
         rect.origin.x *= self.scale
