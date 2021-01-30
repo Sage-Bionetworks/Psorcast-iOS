a018ce633b8ba8a0fd0a71298f89c764618d9fc0
MDP <mike@sdpdigital.com>
2021-01-29 19:50:20 -0800

    Removed debugging flag

--- Psorcast/PsorcastValidation/PsoriasisDrawStepViewController.swift
+++ Psorcast/PsorcastValidation/PsoriasisDrawStepViewController.swift
@@ -49,7 +49,7 @@ open class PsoriasisDrawStepViewController: RSDStepViewController, ProcessorFini
     
     /// This should be turned off when deploying the app, but is useful
     /// for QA to know if the zones and coverage algorithms are working correctly
-    let debuggingZones = true
+    let debuggingZones = false
     
     /// The step for this view controller
     open var drawStep: PsoriasisDrawStepObject? {
