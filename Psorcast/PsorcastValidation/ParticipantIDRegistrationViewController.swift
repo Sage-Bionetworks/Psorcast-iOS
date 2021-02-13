00a79f062e786649b707e470357a91ef1128028d
Mike DePhillips <mike@sdpdigital.com>
2021-02-12 18:21:05 -0800

    Reverted debug code

--- Psorcast/PsorcastValidation/ParticipantIDRegistrationViewController.swift
+++ Psorcast/PsorcastValidation/ParticipantIDRegistrationViewController.swift
@@ -87,7 +87,7 @@ class ParticipantIDRegistrationViewController: RSDStepViewController, UITextFiel
     @IBOutlet public var loadingSpinner: UIActivityIndicatorView!
     
     /// This is helpful for dev, when set, it will auto-set your participant ID to this
-    let autoLoginParticipantId: String? = "mdp"
+    let autoLoginParticipantId: String? = nil
     
     @IBAction func logoutTapped() {
         DispatchQueue.main.async {
