1825bc100bad4ecd1302fc93565f22744dd48c7b
Eric Sieg <eric@sdpdigital.com>
2021-06-23 21:42:09 -0700

    add privacy policy (initial)

--- Psorcast/Psorcast/ProfileTabViewController.swift
+++ Psorcast/Psorcast/ProfileTabViewController.swift
@@ -53,6 +53,7 @@ class ProfileTabViewController: UIViewController, UITableViewDelegate, UITableVi
     public static let deepDiveProfileKey = "DeepDive"
     public static let feedbackProfileKey = "feedback"
     public static let withdrawProfileKey = "withdraw"
+    public static let privacyPolicyKey = "privacyPolicy"
     public static let activityMeasuresKey = "activityMeasures"
     
     override open func viewDidLoad() {
@@ -295,6 +296,12 @@ class ProfileTabViewController: UIViewController, UITableViewDelegate, UITableVi
                 self.showJsonTaskViewControler(jsonName: ProfileTabViewController.feedbackTaskId)
             } else if profileItem.profileItemKey == ProfileTabViewController.withdrawProfileKey {
                 self.showJsonTaskViewControler(jsonName: ProfileTabViewController.withdrawalTaskId)
+            } else if profileItem.profileItemKey == ProfileTabViewController.privacyPolicyKey {
+                let privacyURLString = Bundle.main.path(forResource: "PrivacyPolicy", ofType: "html")
+                let webAction = RSDWebViewUIActionObject(url: privacyURLString!, buttonTitle: "Done")
+                let (_, navVC) = RSDWebViewController.instantiateController(using: AppDelegate.designSystem, action: webAction)
+                navVC.modalPresentationStyle = .formSheet
+                self.present(navVC, animated: true, completion: nil)
             } else if let vc = MasterScheduleManager.shared.instantiateSingleQuestionTreatmentTaskController(for: profileItem.profileItemKey) {
                 vc.delegate = self
                 self.show(vc, sender: self)
