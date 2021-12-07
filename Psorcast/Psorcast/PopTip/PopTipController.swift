51ef9b3090ed32859cc237990d431f0c77234ac9
Eric Sieg <eric@sdpdigital.com>
2021-12-07 12:49:17 -0800

    tweak poptip location

--- Psorcast/Psorcast/PopTip/PopTipController.swift
+++ Psorcast/Psorcast/PopTip/PopTipController.swift
@@ -71,7 +71,7 @@ public class PopTipController: ShowPopTipDelegate {
             NSLog("Measure Tab Landing showPopTip called")
             if let measureTabViewController = viewController as? MeasureTabViewController {
                 let rect = measureTabViewController.topHeader.bounds
-                let popTipRect = rect.offsetBy(dx: (vcBounds.width/2 - rect.width/2), dy: 0)
+                let popTipRect = rect.offsetBy(dx: (vcBounds.width/2 - rect.width/2), dy: -8)
                 popTip.show(text: Localization.localizedString("POPTIP_MEASURE_LANDING"), direction: .down, maxWidth: maxWidth, in: measureTabViewController.view, from: popTipRect)
             }
         case .firstTaskComplete:
